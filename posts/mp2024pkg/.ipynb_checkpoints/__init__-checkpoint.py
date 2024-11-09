660469415117b93e2ed619e1d2e982f764b643c0
miruetoto <miruetoto@gmail.com>
2024-11-10 01:03:17 +0900

    .

---
+++ posts/mp2024pkg/.ipynb_checkpoints/__init__-checkpoint.py
@@ -0,0 +1,10 @@
+from mp2024pkg.core import show_list
+from mp2024pkg.core import show_dict
+from mp2024pkg.core import signature
+from mp2024pkg.core import tree
+from mp2024pkg.core import tab 
+
+def __dir__():
+    keys = dict.fromkeys((globals().keys()))
+    keys.pop("core")
+    return list(keys)
